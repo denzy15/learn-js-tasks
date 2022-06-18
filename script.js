aefaf8c7d1424ae8dcda0038baddce351d78f7b7
denzy15 <mirsh.work@gmail.com>
2022-06-18 16:12:49 +0600

    Part 1 chapter 2.11 tasks#2

--- script.js
+++ script.js
@@ -17,3 +17,11 @@ if (user === null || user == '') {
 } else {
   alert("Я вас не знаю");
 }
+//Task 6
+if (age >= 14 && age <= 90) console.log('В диапазоне');
+
+//Task 7
+
+if (!(age >= 14 && age <= 90)) console.log('Вне диапазона');
+if (age < 14 || age > 90) console.log('Вне диапазона');
+
