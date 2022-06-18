43df7cb048b8f030d6978926c149f9bb117dd1be
denzy15 <mirsh.work@gmail.com>
2022-06-18 15:33:37 +0600

    Part 1 chapter 2.11 tasks

--- script.js
+++ script.js
@@ -1,32 +1,8 @@
-//Task 2
-let jsName = prompt('Какое "официальное" название JavaScript?', "");
-if (jsName == "ECMAScript") {
-  alert("Верно!");
-} else {
-  alert("Не знаете? ECMAScript!");
-}
+//Task 6
+if (age >= 14 && age <= 90) console.log('В диапазоне');
 
-//Task 3
-let num = prompt("Введите число", 0);
-if (num > 0) {
-  alert(1);
-} else if (num < 0) {
-  alert(-1);
-} else {
-  alert(0);
-}
+//Task 7
 
-//Task 4
+if (!(age >= 14 && age <= 90)) console.log('Вне диапазона');
+if (age < 14 || age > 90) console.log('Вне диапазона');
 
-let result = a + b < 4 ? "Мало" : "Много";
-
-//Task 5
-
-let message =
-  login == "Сотрудник"
-    ? "Привет"
-    : login == "Директор"
-    ? "Здравствуйте"
-    : login == ""
-    ? "Нет логина"
-    : "";
