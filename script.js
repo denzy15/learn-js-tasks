a751010af0c6c066c72453d622fb783f692cae8d
denzy15 <mirsh.work@gmail.com>
2022-06-18 16:03:15 +0600

    Part 1 chapter 2.11 tasks#2

--- script.js
+++ script.js
@@ -1,13 +1,3 @@
-//Task 6
-
-/*if (age >= 14 && age <= 90) console.log('В диапазоне');*/
-
-//Task 7
-
-/*if (!(age >= 14 && age <= 90)) console.log('Вне диапазона');
-if (age < 14 || age > 90) console.log('Вне диапазона');*/
-
-//Task 9
 
 let user = prompt("Ваш логин?", "");
 
