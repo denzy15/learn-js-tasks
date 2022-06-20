62bc5885163b2d85cdb4606ebe1e01f823d380c9
denzy15 <mirsh.work@gmail.com>
2022-06-20 14:45:13 +0600

    Part 1 chapter 4.4

--- script.js
+++ script.js
@@ -1,52 +1,38 @@
-//Task 1
-
-let user = {};
-
-user.name = 'John';
-user.surname = 'Smith';
-user.name = 'Pete';
-delete user.name;
-
-//Task 2
-
-function isEmpty(obj){
-  for (const key in obj) {
-    return false;
-  }
-  return true;
-}
-
-console.log(isEmpty({}));
-
 //Task 4
 
-let salaries = {
-  John: 100,
-  Ann: 160,
-  Pete: 130
-}
-
-let sum = 0;
-
-for (const salary in salaries) {
-  sum += salaries[salary];
-}
-
-//Task 5
-
-function  multiplyNumeric(obj) {
-  for (const key in obj) {
-    if (typeof obj[key] === 'number') obj[key] *= 2;
-  }
-  return obj
-}
-
-let menu = {
-  width: 200,
-  height: 300,
-  title: "My menu"
+let calculator = {
+  read: () => {
+    this.a = +prompt("a =", 0);
+    this.b = +prompt("b =", 0);
+  },
+  sum: () => {
+    return this.a + this.b;
+  },
+  mul: () => {
+    return this.a * this.b;
+  },
 };
 
-multiplyNumeric(menu);
+//calculator.read();
+//alert(calculator.sum());
+//alert(calculator.mul());
 
-console.log(menu);
+// Task 5
+
+let ladder = {
+  step: 0,
+  up() {
+    this.step++;
+    return this;
+  },
+  down() {
+    this.step--;
+    return this;
+  },
+  showStep: function () {
+    alert(this.step);
+    return this;
+  },
+};
+
+//ladder.up().up().down().showStep();
