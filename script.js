a9752faf915f4cdc1e7f32683cee8c6df89aa929
denzy15 <mirsh.work@gmail.com>
2022-06-20 15:04:18 +0600

    Part 1 chapter 4.5

--- script.js
+++ script.js
@@ -1,38 +1,36 @@
-//Task 4
+//Task  2
 
-let calculator = {
-  read: () => {
-    this.a = +prompt("a =", 0);
-    this.b = +prompt("b =", 0);
-  },
-  sum: () => {
+function Calculator() {
+  this.read = () => {
+    this.a = +prompt("a?:", 0);
+    this.b = +prompt("b?:", 0);
+  };
+  this.sum = () => {
     return this.a + this.b;
-  },
-  mul: () => {
+  };
+  this.mul = () => {
     return this.a * this.b;
-  },
-};
+  };
+}
 
-//calculator.read();
-//alert(calculator.sum());
-//alert(calculator.mul());
+/*let calculator = new Calculator();
 
-// Task 5
+calculator.read();
+alert("Sum=" + calculator.sum());
+alert("Mul=" + calculator.mul());*/
 
-let ladder = {
-  step: 0,
-  up() {
-    this.step++;
-    return this;
-  },
-  down() {
-    this.step--;
-    return this;
-  },
-  showStep: function () {
-    alert(this.step);
-    return this;
-  },
-};
+//Task 3
 
-//ladder.up().up().down().showStep();
+function Accumulator(startingValue) {
+  this.value = startingValue;
+  this.read = () => {
+    this.value += +prompt('Сколько нужно добавить?', 0); 
+  }
+}
+
+/*let accumulator = new Accumulator(1); // начальное значение 1
+
+accumulator.read(); // прибавит ввод prompt к текущему значению
+accumulator.read(); // прибавит ввод prompt к текущему значению
+alert(accumulator.value);
+*/
