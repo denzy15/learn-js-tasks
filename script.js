6f6d9bc105944d073f074ca3b5129ab97be4a47c
denzy15 <mirsh.work@gmail.com>
2022-06-20 14:31:56 +0600

    Part 1 chapter 4.1

--- script.js
+++ script.js
@@ -1,30 +1,52 @@
+//Task 1
+
+let user = {};
+
+user.name = 'John';
+user.surname = 'Smith';
+user.name = 'Pete';
+delete user.name;
+
 //Task 2
 
-//function checkAge(age) {
-//  return age > 18 ? true : confirm('Родители разрешили?');
-//}
-
-function checkAge(age) {
-  return age > 18 || confirm('Родители разрешили?');
-}
-
-//Task 3
-
-function min(a, b) {
-  return a > b ? b : a;
-}
-
-//Task 4 
-
-function pow(x, n) {
-  let result = 1;
-  for (let i = 0; i < n; i++) {
-    result *= x;
+function isEmpty(obj){
+  for (const key in obj) {
+    return false;
   }
-  return result;
+  return true;
 }
 
-let x = prompt('Введите х', 1);
-let n = prompt('Введите n', 1);
+console.log(isEmpty({}));
 
-if (n >= 1) alert(pow(x, n));
+//Task 4
+
+let salaries = {
+  John: 100,
+  Ann: 160,
+  Pete: 130
+}
+
+let sum = 0;
+
+for (const salary in salaries) {
+  sum += salaries[salary];
+}
+
+//Task 5
+
+function  multiplyNumeric(obj) {
+  for (const key in obj) {
+    if (typeof obj[key] === 'number') obj[key] *= 2;
+  }
+  return obj
+}
+
+let menu = {
+  width: 200,
+  height: 300,
+  title: "My menu"
+};
+
+multiplyNumeric(menu);
+
+console.log(menu);
