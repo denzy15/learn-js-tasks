f0a8d31aa8705a826df8030fbf1ad9cbaac3743a
denzy15 <mirsh.work@gmail.com>
2022-06-21 15:54:00 +0600

    Part 1 chapter 5.12

--- script.js
+++ script.js
@@ -1,82 +1,34 @@
 //task 1
 
-/*let date = new Date(2012, 1, 20, 3, 12);
-alert(date);*/
+let user = {
+  name: "Василий Иванович",
+  age: 35,
+};
+
+const newUser = JSON.parse(JSON.stringify(user));
+console.log(newUser);
 
 //task 2
 
-function getWeekDay(date) {
-  let daysOfWeek = ["ВС", "ПН", "ВТ", "СР", "ЧТ", "ПТ", "СБ"];
-  const day = date.getDay();
-  return daysOfWeek[day];
-}
+let room = {
+  number: 23,
+};
 
-// task 3
+let meetup = {
+  title: "Совещание",
+  occupiedBy: [{ name: "Иванов" }, { name: "Петров" }],
+  place: room,
+};
 
-function getLocalDay(date) {
-  let day = date.getDay();
-  if (day === 0) {
-    day = 7;
-  }
-  return day;
-}
+room.occupiedBy = meetup;
+meetup.self = meetup;
 
-//task 4
-function getDateAgo(date, days) {
-  let newDate = new Date(date);
-  newDate.setDate(date.getDate() - days);
-  return newDate.getDate();
-}
-
-//task 5
-
-function getLastDayOfMonth(year, month) {
-  return new Date(year, month + 1, 0).getDate();
-}
-
-//task 6
-
-function getSecondsToday() {
-  let now = new Date();
-  let today = new Date(now.getFullYear(), now.getMonth(), now.getDate());
-  return Math.round((now - today) / 1000);
-}
-
-//task 7
-
-function getSecondsToTomorrow() {
-  let now = new Date();
-  let tomorrow = new Date(now.getFullYear(), now.getMonth(), now.getDate() + 1);
-  return Math.round((tomorrow - now) / 1000);
-}
-
-//task 8
-
-function formatDate(date) {
-  let msec = new Date() - date;
-  if (msec < 1000) {
-    return "прямо сейчас";
-  }
-  let sec = Math.floor(msec / 1000);
-  if (sec < 60) {
-    return `${sec} секунд назад`;
-  }
-
-  let min = Math.floor(sec / 60);
-  if (min < 60) {
-    return `${min} минут назад`;
-  }
-
-  let newDate = new Date(date);
-  return `${newDate.getDate()}.${
-    newDate.getMonth() < 10
-      ? "0" + (newDate.getMonth() + 1)
-      : newDate.getMonth() + 1
-  }.${newDate.getFullYear()}, ${
-    newDate.getHours() < 10 ? "0" + newDate.getHours() : newDate.getHours()
-  }:${
-    newDate.getMinutes() < 10
-      ? "0" + newDate.getMinutes()
-      : newDate.getMinutes()
-  }`;
-}
+console.log(
+  JSON.stringify(
+    meetup,
+    function replacer(key, value) {
+      return key !== "" && value === meetup ? undefined : value;
+    },
+    " "
+  )
+);
