139653eba219156d4dbc40c06b6a7e48eb1ed986
denzy15 <mirsh.work@gmail.com>
2022-06-21 13:46:20 +0600

    Part 1 chapter 5.7

--- script.js
+++ script.js
@@ -1,107 +1,23 @@
-//Task 1
-
-function camelize(str) {
-  let newStr = str.split("-");
-  for (let i = 1; i < newStr.length; i++) {
-    newStr[i] = newStr[i][0].toUpperCase() + newStr[i].slice(1);
-  }
-  return newStr.join("");
-}
-
-//Task 2
-
-function filterRange(arr, a, b) {
-  return arr.filter(num >= num >= a && num <= b);
-}
-
-//task 3
-
-function filterRangeInPlace(arr, a, b) {
-  for (let i = 0; i < arr.length; i++) {
-    if (arr[i] < a || arr[i] > b) {
-      arr.splice(i, 1);
-      i--;
-    }
-  }
-}
-
-//task 4
-
-//arr.sort((a, b) => b - a);
-
-//task 5
-
-function copySorted(arr) {
-  return arr.slice().sort();
-}
-
-//task 6
-
-function Calculator() {
-  this.calculate = function(str) {
-    let split = str.split(" "),
-      a = +split[0],
-      method = split[1],
-      b = +split[2];
-
-    if (!this.methods[method] || !isFinite(a) || !isFinite(b)) {
-      return 'Что-то пошло не так';
-    }
-
-    return this.methods[method](a, b);
-  };
-
-  this.methods = {
-    "-": (a, b) => a - b,
-    "+": (a, b) => a + b,
-  };
-
-  this.addMethod = function(method, f) {
-    this.methods[method] = f;
-  };
-}
-
-//task 7
-
-//let names = users.map(item => item.name)
-
-//task 8
-
-/*let usersMapped = users.map(item => ({
-  fullName : item.name + ' ' + item.surname,
-  id: item.id
-}))
-*/
-
-//task 9
-
-function sortByAge(arr){
-  arr.sort((a, b) => a.age > b.age ? 1 : -1)
-}
-
-//task 10
-
-function shuffle(arr) {
-  arr.sort(() => Math.random() - 0.5);
-}
-
-//task 11
-
-function getAverageAge(users) {
-  let sum = 0;
-  users.forEach(user => sum += user.age);
-  return sum / users.length;
-}
-
-//task 12
+//task 1
 
 function unique(arr) {
-  const newArr = [];
-  for (const item of arr) {
-    if (!newArr.includes(item)) {
-      newArr.push(item);
-    }
-  }
-  return newArr;
+  let set = new Set(arr);
+  return Array.from(set);
 }
+
+let values = ["Hare", "Krishna", "Hare", "Krishna",
+  "Krishna", "Krishna", "Hare", "Hare", ":-O"
+];
+
+//task 2
+
+let arr = ["nap", "teachers", "cheaters", "PAN", "ear", "era", "hectares"];
+
+function aclean(arr){
+  let set = new Set();
+  arr.forEach(str => set.add(str.toLowerCase().split('').sort().join('')));
+  return Array.from(set);
+}
+
+//Task 3
 
