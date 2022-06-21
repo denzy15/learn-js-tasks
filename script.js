82cbee9fd0df09c96e73fe947fc24f992fe89a45
denzy15 <mirsh.work@gmail.com>
2022-06-22 02:03:23 +0600

    Part 1 chapter 6.6

--- script.js
+++ script.js
@@ -1,71 +1,52 @@
-//task 5
+//task 1
 
-function sum(a) {
-  return function (b) {
-    return a + b;
+function makeCounter() {
+  let count = 0;
+  function counter() {
+    return count++;
+  }
+  counter.set = function (n) {
+    count = n;
   };
+  counter.decrease = () => {
+    return count--;
+  };
+
+  return counter;
 }
 
-//console.log(sum(6)(9))
+/*let counter = makeCounter();
 
-//task 5
+console.log(counter()); // 0
+console.log(counter()); // 1
 
-function inBetween(a, b) {
-  return function (num) {
-    if (num >= a && num <= b) return true;
-    return false;
-  };
-}
+counter.set(10); // установить новое значение счётчика
 
-function inArray(arr) {
-  return function (num) {
-    return arr.includes(num);
-  };
-}
+console.log(counter()); // 10
 
-/*let arr = [1, 2, 3, 4, 5, 6, 7];
+counter.decrease(); // уменьшить значение счётчика на 1
 
-console.log(arr.filter(inBetween(3, 6))); // 3,4,5,6
-
-console.log(arr.filter(inArray([1, 2, 10]))); // 1,2*/
-
-//task 6
-
-function byField(key) {
-  return function (a, b) {
-    return a[key] > b[key] ? 1 : -1;
-  };
-}
-
-let users = [
-  { name: "John", age: 20, surname: "Johnson" },
-  { name: "Pete", age: 18, surname: "Peterson" },
-  { name: "Ann", age: 19, surname: "Hathaway" },
-];
-
-/*
-console.log(users.sort(byField("name")));
-console.log(users.sort(byField("age")));
+console.log(counter()); // 10 (вместо 11)
 */
 
-//task 7
+//task 2
 
-function makeArmy() {
-  let shooters = [];
+function sum(a) {
+  let currentSum = a;
 
-  for (let i = 0; i < 10; i++) {
-    let shooter = function () {
-      // функция shooter
-      console.log(i);
-      // должна выводить порядковый номер
-    };
-    shooters.push(shooter);
-    debugger;
+  function f(b) {
+    currentSum += b;
+    return f;
   }
-  return shooters;
+
+  f.toString = function () {
+    return currentSum;
+  };
+
+  return f;
 }
 
-let army = makeArmy();
+alert(sum(2)(9));
 
-army[0](); // у 0-го стрелка будет номер 10
-army[5]();
+//let x = sum(1)(2);
+//console.log( x().toString() );
