32a4b8251dc0240a34d79587271c8db7d91f304e
denzy15 <mirsh.work@gmail.com>
2022-06-22 18:11:00 +0600

    Part 1 chapter 8.2

--- script.js
+++ script.js
@@ -1,49 +1,12 @@
-//task 2
-
-let head = {
-  glasses: 1,
-};
-
-let table = {
-  pen: 3,
-  __proto__: head,
-};
-
-let bed = {
-  sheet: 1,
-  pillow: 2,
-  __proto__: table,
-};
-
-let pockets = {
-  money: 2000,
-  __proto__: bed,
-};
-
-//console.log(pockets.pen);
-
-//task 4
-
-let hamster = {
-  stomach: [],
-
-  eat(food) {
-    this.stomach.push(food);
-  },
-};
-
-let speedy = {
-  stomach: [],
-  __proto__: hamster,
-};
-
-let lazy = {
-  stomach: [],
-  __proto__: hamster,
-};
+function User(name) {
+  this.name = name;
+  console.log(name);
+}
 
 
-speedy.eat("apple");
-console.log(speedy.stomach); // apple
+let obj = new User("John");
 
-console.log(lazy.stomach); // empty
+//Если свойство "User.prototype.constructor" не будет перезаписано, то всё будет работать правильно 
+//User.prototype.constructor = {};
+
+let obj2 = new obj.constructor("Pete");
