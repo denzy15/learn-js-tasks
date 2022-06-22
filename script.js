c99907732f87be6f24f7c3f2d6f3bdb7115c8fec
denzy15 <mirsh.work@gmail.com>
2022-06-23 00:50:43 +0600

    Part 1 chapter 9.3

--- script.js
+++ script.js
@@ -1,55 +1,12 @@
 //task 1
 
-/*class Animal {
+class Rabbit extends Object {
   constructor(name) {
+    super();
     this.name = name;
   }
 }
 
-class Rabbit extends Animal {
-  constructor(name) {
-    super(name);
-    this.created = Date.now();
-  }
-}
+let rabbit = new Rabbit("Кроль");
 
-let rabbit = new Rabbit("Белый кролик");
-console.log(rabbit.name);
-*/
-//task 2
-
-class Clock {
-  constructor({ template }) {
-    this.template = template;
-  }
-
-  render() {
-    let date = new Date();
-
-    let hours = date.getHours();
-    if (hours < 10) hours = '0' + hours;
-
-    let mins = date.getMinutes();
-    if (mins < 10) mins = '0' + mins;
-
-    let secs = date.getSeconds();
-    if (secs < 10) secs = '0' + secs;
-
-    let output = this.template
-      .replace('h', hours)
-      .replace('m', mins)
-      .replace('s', secs);
-
-    console.log(output);
-  }
-
-  stop() {
-    clearInterval(this.timer);
-  }
-
-  start() {
-    this.render();
-    this.timer = setInterval(() => this.render(), 1000);
-  }
-}
-
+alert(rabbit.hasOwnProperty("name"));
