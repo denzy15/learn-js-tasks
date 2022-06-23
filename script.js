841969c045eb21ef7a1ded7f8e6c13e0933f9c53
denzy15 <mirsh.work@gmail.com>
2022-06-23 14:34:39 +0600

    Part 2 chapter 1.5

--- script.js
+++ script.js
@@ -1,24 +1,9 @@
-//Таблицу с id="age-table"
-const ageTable = document.querySelector("#age-table");
-
-//Все элементы label внутри этой таблицы (их три)
-const labels = ageTable.getElementsByTagName("label");
-
-//Первый td в этой таблице (со словом «Age»).
-const td = ageTable.querySelector("td");
-
-//Форму form с именем name="search".
-const form = document.querySelector("form");
-
-//Инпуты в form
-let inputs = form.getElementsByTagName('input');
-
-//Первый input в этой форме.
-const firstInput = inputs[0];
-
-//Последний input в этой форме.
-const secondInput = inputs[inputs.length-1];
-
-
+//task 1
+const listLis = document.querySelectorAll("li");
+for (let li of listLis) {
+  let name = li.firstChild.textContent.replace(/\s+/g, "");
+  let count = li.getElementsByTagName("li").length;
+  console.log(`${name}: ${count}`);
+}
 
 
